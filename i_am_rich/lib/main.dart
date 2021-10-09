66b0dc19f7bda94e640576f4f7a62778a581b196
Novelworm <mikhaya.jrh@gmail.com>
2021-10-09 22:50:38 +0700

    Scaffolding a Flutter App

--- i_am_rich/lib/main.dart
+++ i_am_rich/lib/main.dart
@@ -4,8 +4,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello World"),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: Text("I Am Rich"),
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage(
+                "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"),
+          ),
+        ),
       ),
     ),
   );
