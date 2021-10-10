36f126b1e76bbdf4e915f3486da07350806c2751
Novelworm <mikhaya.jrh@gmail.com>
2021-10-10 22:33:08 +0700

    Adding Gesture Detection with Flutter Button Widges

--- dicee-flutter/lib/main.dart
+++ dicee-flutter/lib/main.dart
@@ -23,14 +23,26 @@ class DicePage extends StatelessWidget {
         children: [
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                // TextButton has default padding 8.0
+                onPressed: () {
+                  print("Left button got pressed.");
+                },
+                child: Image.asset('images/dice1.png'),
+              ),
             ),
           ),
           Expanded(
             child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Image.asset('images/dice1.png'),
+              padding: const EdgeInsets.all(8.0),
+              child: TextButton(
+                // TextButton has default padding 8.0
+                onPressed: () {
+                  print("Right button got pressed.");
+                },
+                child: Image.asset('images/dice1.png'),
+              ),
             ),
           ),
         ],
