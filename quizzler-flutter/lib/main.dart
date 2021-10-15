f908904f41a42a87dee645b052ee9a7f86208276
Novelworm <mikhaya.jrh@gmail.com>
2021-10-15 17:30:47 +0700

    Abstraction in Action

--- quizzler-flutter/lib/main.dart
+++ quizzler-flutter/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:quizzler/question.dart';
+import 'package:quizzler/quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -27,20 +29,6 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Icon> scoreKeeper = [];
-  List<Question> questionBank = [
-    Question(
-      questionText: 'You can lead a cow down stairs but not up stairs.',
-      questionAnswer: false,
-    ),
-    Question(
-      questionText: 'Approximately one quarter of human bones are in the feet.',
-      questionAnswer: true,
-    ),
-    Question(
-      questionText: 'A slug\'s blood is green.',
-      questionAnswer: true,
-    ),
-  ];
   int questionNumber = 0;
 
   @override
@@ -55,7 +43,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber].questionText,
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -80,8 +68,9 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                // quizBrain.questionBank[questionNumber].questionAnswer = true;
                 bool correctAnswer =
-                    questionBank[questionNumber].questionAnswer;
+                    quizBrain.questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == true)
                   print("user got it right!");
                 else
@@ -109,7 +98,7 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
                 bool correctAnswer =
-                    questionBank[questionNumber].questionAnswer;
+                    quizBrain.questionBank[questionNumber].questionAnswer;
                 if (correctAnswer == false)
                   print("user got it right!");
                 else
--- quizzler-flutter/lib/question.dart
+++ quizzler-flutter/lib/question.dart
@@ -1,8 +1,6 @@
-import 'package:flutter/cupertino.dart';
-
 class Question {
   String questionText;
   bool questionAnswer;
 
-  Question({@required this.questionText, @required this.questionAnswer});
+  Question(this.questionText, this.questionAnswer);
 }
