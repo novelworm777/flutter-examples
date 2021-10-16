a30639c710b9dbdca2a1dfab2aa58b25c7c1293b
Novelworm <mikhaya.jrh@gmail.com>
2021-10-16 16:19:28 +0700

    How to Refactor Flutter Widgets

--- bmi-calculator-flutter/lib/input_page.dart
+++ bmi-calculator-flutter/lib/input_page.dart
@@ -12,8 +12,57 @@ class _InputPageState extends State<InputPage> {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: [
+          Expanded(
+            child: Row(
+              children: [
+                ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+                ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ReusableCard(
+              colour: Color(0xFF1D1E33),
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+                ReusableCard(
+                  colour: Color(0xFF1D1E33),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  final Color colour;
+
+  ReusableCard({@required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
