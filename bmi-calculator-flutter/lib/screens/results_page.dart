30d6b79d2d310c43da894ad21f42ace133e85bb6
Novelworm <mikhaya.jrh@gmail.com>
2021-10-17 16:04:17 +0700

    Designing the Result Page

---
+++ bmi-calculator-flutter/lib/screens/results_page.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../components/bottom_button.dart';
+import '../constants.dart';
+import '../components/reusable_card.dart';
+
+class ResultsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Calculator"),
+      ),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
+              child: Text("Your Result", style: kTitleTextStyle),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardColour,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("Normal", style: kResultTextStyle),
+                  Text("18.3", style: kBMITextStyle),
+                  Text(
+                    "Your BMI result is quite low, you should eat more!",
+                    style: kBodyTextStyle,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          BottomButton(
+            buttonTitle: "RE-CALCULATE",
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
